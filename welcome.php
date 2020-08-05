50a1b2b2a2fe385680b49d100866af9fb73db211
SachalaH <59394760+SachalaH@users.noreply.github.com>
2020-08-05 14:20:15 +0530

    Basic echo in php

---
+++ welcome.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP welcome</title>
+</head>
+
+<body>
+    <?php
+    // echo "WELCOME.";
+    // phpinfo();
+    $s1 = "Hello";
+    $s2 = "World";
+    echo "String 1 is $s1<br/>"; // Double quotes print string and 
+                                //if any variables in it as well
+    echo 'String 2 is $s2 '.$s2; //Single quotes print the raw string 
+                                //String concat can be possible with a dot .
+    ?>
+</body>
+
+</html>
