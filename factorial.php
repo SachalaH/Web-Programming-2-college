c78ad8b898860f7204424fed212db213481cd899
SachalaH <59394760+SachalaH@users.noreply.github.com>
2020-08-11 17:15:57 +0530

    Added headings and minor conditions tweak

--- factorial.php
+++ factorial.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Factorial</title>
+    <title>Factorial results</title>
 </head>
 
 <body>
@@ -13,11 +13,19 @@
 
     $factorial = 1;
     $number = $_GET["number"];
+    if($number<0){
+        echo "<h1>Number is less than zero.</h1>";
+    }
+    elseif($number==0){
+        echo "<h1>Factorial of 0 is 1.</h1>";
+    }
+    else{
     for($i=1;$i<=$number;$i++){
         $factorial *= $i;
     }
 
     echo "<h1>The number is $number</h1><br/><h1>The factorial for same is $factorial</h1>";
+}
 
     ?>
 
--- fibonacci.php
+++ fibonacci.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Factorial</title>
+    <title>Fibonacci result</title>
 </head>
 
 <body>
