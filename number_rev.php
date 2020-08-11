fc5e8e349f7cec8730ffe1ba8ce481d9f61b4dba
SachalaH <59394760+SachalaH@users.noreply.github.com>
2020-08-11 17:06:20 +0530

    Reverse of number

---
+++ number_rev.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Factorial</title>
+</head>
+
+<body>
+
+    <?php
+    $num = $_GET["number"];
+    $res = "";
+    echo "<h1>Submitted number is : $num</h1>";
+
+    while((int)$num!=0){
+        $mod = $num%10;
+        $res .= "$mod";
+        $num = $num/10;
+    }
+    
+    echo "<h1>Reverse of number is : $res</h1>";
+
+    
+
+    ?>
+
+</body>
+
+</html>
